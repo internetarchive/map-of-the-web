3f9ce4c6d2322f5c978ce34c235d54b8fc0c870a
verseccccccc <2827572079@qq.com>
2018-06-13 18:06:56 -0400

    draw second figure about the webpages with or without digest changes

--- map-of-the-web/motw/motw/producing.py
+++ map-of-the-web/motw/motw/producing.py
@@ -30,6 +30,26 @@ def Frequency(snapshot_list):
 	frequency_list.append(number)
 	return [timestamp_list, frequency_list]
 
+def test_use(snapshot_list):
+	digest_use = dict()
+	digest_list = []
+	timestamp_list = []
+	usefulnes_list = []
+	digest_use[snapshot_list[0]['digest']] = snapshot_list[0]['timestamp']
+	digest_list.append(snapshot_list[0]['digest'])
+	timestamp_list.append(snapshot_list[0]['timestamp'])
+	usefulnes_list.append(1)
+	for i in range(1, len(snapshot_list)):
+		timestamp_list.append(snapshot_list[i]['timestamp'])
+		if snapshot_list[i]['digest'] in digest_use.keys():
+			snapshot_list[i]['usefulness'] = 0
+			snapshot_list[i]['same_as'] = digest_use[snapshot_list[i]['digest']]
+		else:
+			digest_use[snapshot_list[i]['digest']] = snapshot_list[i]['timestamp']
+			digest_list.append(snapshot_list[i]['digest'])
+		usefulnes_list.append(snapshot_list[i]['usefulness'])
+	return [timestamp_list, usefulnes_list]
+
 def draw_broken_line(data, dw, dh):
 	tools = "pan,box_zoom,reset,save"
 	#path = r"bokehTest.html"
@@ -40,6 +60,23 @@ def draw_broken_line(data, dw, dh):
 	s1.line(x, y, color="navy", line_width=2)
 	s1.text(x, y, text=y, text_baseline="middle", text_align="center")
 	return s1
+
+def test_use_draw(data, duplist, dw, dh):
+	one = []
+	for i in range(len(data)):
+		one.append(0.5)
+	xd = pd.to_datetime(data)
+	print(xd[2])
+	s1 = figure(plot_width=dw, plot_height=dh, x_axis_type="datetime")
+	s1.vbar(x=xd, width=3, bottom=0, top=one, color="firebrick")
+	s1.vbar(x=xd, width=3, bottom=0, top=duplist, color="navy")
+	return s1
+
+'''def huadiandian(data, dw, dh):
+	one = []
+	for i in range(len(data)):
+		one.append(1)
+	s1 = figure(plot_width=dw, plot_height=dh)'''
 
 def download(str):
 	res = req.get("http://web.archive.org/cdx/search/cdx?url="+str)
@@ -52,11 +89,40 @@ def download(str):
 		snapshot_list.append(snap)
 	return snapshot_list
 
+def duplicatelist(snapshot_list):
+	digest_use = dict()
+	digest_list = []
+	timestamp_list = []
+	usefulnes_list = []
+	digest_use[snapshot_list[0]['digest']] = snapshot_list[0]['timestamp']
+	digest_list.append(snapshot_list[0]['digest'])
+	timestamp_list.append(snapshot_list[0]['timestamp'])
+	usefulnes_list.append(1)
+	for i in range(1, len(snapshot_list)):
+		timestamp_list.append(snapshot_list[i]['timestamp'])
+		if snapshot_list[i]['digest'] in digest_use.keys():
+			snapshot_list[i]['usefulness'] = 0
+			snapshot_list[i]['same_as'] = digest_use[snapshot_list[i]['digest']]
+		else:
+			digest_use[snapshot_list[i]['digest']] = snapshot_list[i]['timestamp']
+			digest_list.append(snapshot_list[i]['digest'])
+		usefulnes_list.append(snapshot_list[i]['usefulness'])
+	dl=[]
+	for i in range(len(snapshot_list)):
+		dl.append(snapshot_list[i]["usefulness"])
+	#print(dl)
+	return dl
+
 def processing(str):
 	snapshot_list = download(str)
+	duplist = duplicatelist(snapshot_list)
 	data = Frequency(snapshot_list)
 	picture1 = draw_broken_line(data, 1280, 480)
-	return [picture1]
+	use = test_use(snapshot_list)
+	timestamp_list = use[0]
+	#print(timestamp_list[1])
+	picture2 = test_use_draw(timestamp_list, duplist, 1280, 480)
+	return [picture1, picture2]
 
 def Count(str):
 	snapshot_list = download(str)
--- map-of-the-web/motw/motw/view.py
+++ map-of-the-web/motw/motw/view.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from motw.producing import processing
 from motw.producing import Count
+from motw.producing import download
 from bokeh.resources import CDN
 from bokeh.embed import components
 
@@ -11,8 +12,10 @@ def test(request):
 		fig = processing(query)
 		total_number = Count(query)
 		script1, div1 = components(fig[0], CDN)
+		script2, div2 = components(fig[1], CDN)
 		return render(request, 'test01.html', \
 									{'script1': script1, 'div1': div1, \
+									 'script2': script2, 'div2': div2, \
 									 'results': query, 'total_number': total_number})
 	else:
 		results = []
