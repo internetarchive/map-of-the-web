889840348e86ebffa4d4d543a70d06ff0417d786
verseccccccc <2827572079@qq.com>
2018-06-13 17:55:06 -0400

    draw first figure about the frequency of crawlering from the domain name

--- map-of-the-web/motw/motw/producing.py
+++ map-of-the-web/motw/motw/producing.py
@@ -6,6 +6,41 @@ import requests as req
 from datetime import datetime
 import pandas as pd
 
+def timestamp2date(x):
+	return x[0:4]+"-"+x[4:6]+"-01"
+
+def Frequency(snapshot_list):
+	string = snapshot_list[0]['timestamp']
+	time = timestamp2date(string)
+	timestamp_list = []
+	frequency_list = []
+	frequency = dict()
+	frequency[time] = 1
+	timestamp_list.append(time)
+	number = 1
+	for i in range(1, len(snapshot_list)):
+		if (timestamp2date(snapshot_list[i]['timestamp']) != time):
+			time = timestamp2date(snapshot_list[i]['timestamp'])
+			timestamp_list.append(time)
+			frequency_list.append(number)
+			number = 1
+		else:
+			number += 1
+			frequency[time] = number
+	frequency_list.append(number)
+	return [timestamp_list, frequency_list]
+
+def draw_broken_line(data, dw, dh):
+	tools = "pan,box_zoom,reset,save"
+	#path = r"bokehTest.html"
+	#output_file(path)  
+	x = pd.to_datetime(data[0])
+	y = data[1]
+	s1 = figure(plot_width=dw, plot_height=dh, title=None, x_axis_type="datetime")
+	s1.line(x, y, color="navy", line_width=2)
+	s1.text(x, y, text=y, text_baseline="middle", text_align="center")
+	return s1
+
 def download(str):
 	res = req.get("http://web.archive.org/cdx/search/cdx?url="+str)
 	snapshots = res.text.split('\n')
@@ -16,6 +51,12 @@ def download(str):
 			snap = Snapshot(snapshot_items[0], snapshot_items[1], snapshot_items[2], snapshot_items[3], snapshot_items[4], snapshot_items[5], snapshot_items[6])
 		snapshot_list.append(snap)
 	return snapshot_list
+
+def processing(str):
+	snapshot_list = download(str)
+	data = Frequency(snapshot_list)
+	picture1 = draw_broken_line(data, 1280, 480)
+	return [picture1]
 
 def Count(str):
 	snapshot_list = download(str)
--- map-of-the-web/motw/motw/view.py
+++ map-of-the-web/motw/motw/view.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from motw.producing import processing
 from motw.producing import Count
+from bokeh.resources import CDN
+from bokeh.embed import components
 
 def test(request):
 	query = request.GET.get('q','') 
 	if query:
+		fig = processing(query)
 		total_number = Count(query)
+		script1, div1 = components(fig[0], CDN)
 		return render(request, 'test01.html', \
-									{'results': query, 'total_number': total_number})
+									{'script1': script1, 'div1': div1, \
+									 'results': query, 'total_number': total_number})
 	else:
 		results = []
 		return render_to_response('test01.html', {'results': results})
