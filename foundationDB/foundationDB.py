37fffcf60cfa4dfc6c0b1198886ba32842f6ac2e
verseccccccc <2827572079@qq.com>
2018-07-11 23:20:33 -0400

    Speed up 1.5✖x faster of storing data into foundationDB database

--- foundationDB/foundationDB.py
+++ foundationDB/foundationDB.py
@@ -9,33 +9,41 @@ import json
 from functools import partial
 from multiprocessing import Pool
 
-fdb.api_version(520)
-db = fdb.open()
+fdb.api_version(510)
+db = fdb.open('fdb.cluster')
 
 domain_index = fdb.Subspace(('domain-index',))
 similarweb = fdb.Subspace(('similarweb',))
 
-path = "/home/ubuntu/foundationDB/test"
+path = "/home/zcheng/foundationDB/Build/all"
 files = os.listdir(path)
 #print ('hello')
 #print (files)
 
 @fdb.transactional
-def add_url(tr, url, date, pos):
-    tr[fdb.Subspace((pos, ))[url]] = str.encode(date)
+def add_url(tr, url, date):
+    #tr[fdb.Subspace((pos, ))[url]] = str.encode(date)
+    tr[domain_index.pack((url,))] = str.encode(date)
 
-def store_file_domain_index(domain, create_date):
-    add_url(db, domain, create_date, 'domain-index')
+def store_file_archive(domain, create_date):
+    #print ("hhhh")
+    add_url(db, domain, create_date)
+    #print("hhhhh")
 
 def read_file(filename):
     #print ("hello")
-    csvFile = pd.read_csv("/home/ubuntu/foundationDB/test/" + filename, usecols = ['domain', 'create_date'])
+    csvFile = pd.read_csv("/home/zcheng/foundationDB/Build/all/" + filename, usecols = ['domain', 'create_date'])
+    print (len(csvFile))
     for i in range(0, len(csvFile)):
-        store_file_domain_index(csvFile['domain'][i], str(csvFile['create_date'][i]))
+        print (i)
+        #print(csvFile['domain'][i])
+        #print(str(csvFile['create_date'][i]))
+        store_file_archive(csvFile['domain'][i], str(csvFile['create_date'][i]))
         #print (i)
-        value = str(db[fdb.Subspace(('domain-index', ))[csvFile['domain'][i]]])
+        #value = str(db[fdb.Subspace(('domain-index', ))[csvFile['domain'][i]]])
         #print (csvFile['domain'][i])
-        #print (value)        
+        #print (value)
+    #print ('23333')        
     return csvFile
 
 def read_head(filename):
@@ -44,8 +52,10 @@ def read_head(filename):
 
 def multiple_processors(filename_list):
     #with Pool(1) as p:
-        #p.map(read_file, filename_list)
+     #   p.map(read_file, filename_list)
     for i in range(0, len(files)):
+        print (i)
+        print (files[i])
         read_file(files[i])
 
 def multiple(filename_list):
@@ -53,8 +63,9 @@ def multiple(filename_list):
         print(p.map(read_file, filename_list))
 
 if __name__ == '__main__':
+    #read_file("/home/zcheng/foundationDB/test/ae.csv")
     multiple_processors(files)
-    '''csvFile = pd.read_csv("/home/ubuntu/foundationDB/test/" + "aaa.csv", usecols = ['domain', 'create_date'])
+    '''csvFile = pd.read_csv("/home/zcheng/foundationDB/small/" + "aaa.csv", usecols = ['domain', 'create_date'])
     for i in range(0, len(csvFile) - 1):
         store_file_archive(csvFile['domain'][i], str(csvFile['create_date'][i]))
         print (i)'''
